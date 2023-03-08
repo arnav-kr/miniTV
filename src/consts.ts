e6371e696175e912f21800f7608a888f667584f6
arnav-kr <72879799+arnav-kr@users.noreply.github.com>
2023-03-08 18:59:55 +0530

    ➕Add Help Message

---
+++ src/consts.ts
@@ -0,0 +1,33 @@
+// significant text in cyan
+// author in unique style with solid background
+// notes in secondary color
+export const help = `
+\x1b[36m
+            _       _ _________      __
+           (_)     (_)__   __\\ \\    / /
+  _ __ ___  _ _ __  _   | |   \\ \\  / / 
+ | '_ \` _ \\| | '_ \\| |  | |    \\ \\/ /  
+ | | | | | | | | | | |  | |     \\  /   
+ |_| |_| |_|_|_| |_|_|  |_|      \\/    
+                                                                     
+\x1b[0m
+Welcome to \x1b[36mminiTV\x1b[0m
+A tool to download videos from amazon minitv
+
+\x1b[90mNote: This tool is just a proxy to youtube-dl and requires youtube-dl to be installed on your system to work.
+All options for youtube-dl are supported here too.\x1b[0m
+
+\x1b[36mUsage:\x1b[0m minitv <url> [options]
+
+\x1b[36mOptions:\x1b[0m
+  -h, --help, help:  Show this help message and exit
+
+  all other provided options are passed to youtube-dl
+  run \x1b[36myoutube-dl --help\x1b[0m for help with youtube-dl
+
+\x1b[36mExample:\x1b[0m minitv https://www.amazon.in/minitv/tp/0478a3f9-e35d-40d4-97e8-e09684007fa6 -f bestvideo+bestaudio
+
+\x1b[36mGithub:\x1b[0m https://github.com/arnav-kr/minitv
+
+\x1b[36mAuthor:\x1b[0m \x1b[46m\x1b[30m@arnav-kr\x1b[0m
+`;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import fetch from "./fetchInfo";
 import child_process from "child_process";
+import { help } from "./consts";
 
 // make a cli tool that is proxy to youtube-dl just insert the url using fetchInfo
 
@@ -8,10 +9,26 @@ let url = process.argv[2];
 let args = process.argv.slice(3);
 
 ; (async () => {
+  // if no url is provided show help
+  if (!url) {
+    console.log(help);
+    process.exit(0);
+  }
+  // if help is provided show help
+  if (url === "-h" || url === "--help" || url === "help") {
+    console.log(help);
+    process.exit(0);
+  }
 
-  let info = await fetch(url);
-  let { playbackData } = info;
-  let mpdURL = playbackData.playbackAssets.dash.manifestUrl;
+  let mpdURL: string = "";
+  try {
+    let info = await fetch(url);
+    let { playbackData } = info;
+    mpdURL = playbackData.playbackAssets.dash.manifestUrl;
+  } catch (err) {
+    console.error((err as Error).message);
+    process.exit(1);
+  }
 
   console.log(`Dash URL: \x1b[36m${mpdURL}\x1b[0m`);
 
